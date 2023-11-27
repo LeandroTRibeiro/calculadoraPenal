b0e4bb7e2f785012dd5324f0b75c51d522cbe36b
Leandro Thiago Ribeiro <111009157+LeandroTRibeiro@users.noreply.github.com>
2023-11-27 09:01:49 -0300

    style: add responsiveness to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,14 +9,14 @@ export const Home = () => {
         <div className="w-screen h-screen">
             <NavigationHeader />
             <BackgroundVideo />
-            <main className="h-main mobile-g:h-main-mobile flex items-center px-10 mobile-g:px-3 py-5 mobile-g:py-0 gap-7 tablet-m:flex-col-reverse tablet-m:justify-center">
+            <main className="h-main mobile-g:h-main-mobile flex items-center px-10 mobile-g:px-3 tablet-m:flex-col-reverse tablet-m:justify-center">
                 <div className="flex-1 flex flex-col gap-5 tablet-m:flex-initial">
                     <div className="flex flex-col gap-5 laptop-p:gap-3 mobile-g:gap-2 mobile-m:gap-1">
                         <h2 className="text-xl tablet-m:text-lg mobile-m:text-base font-semibold">Bem-Vindo(a) à Calculadora Penal!</h2>
                         <p className="text-base tablet-m:text-sm">A Calculadora Penal é uma ferramenta projetada para auxiliar profissionais, estudantes e entusiastas do Direito Penal. Esta plataforma digital busca facilitar e agilizar o processo de cálculo de penas, proporcionando resultados precisos e confiáveis de maneira rápida e intuitiva.</p>
                         <h3 className="text-xl tablet-m:text-lg mobile-m:text-base font-semibold">Como Funciona?</h3>
                         <p className="text-base tablet-m:text-sm">Basta inserir os dados necessários nos campos correspondentes, e a Calculadora Penal irá guiá-lo através das etapas da dosimetria penal, desde a pena base até a pena definitiva. Além disso, você pode contar com recursos adicionais e dicas úteis para entender melhor cada fase do cálculo.</p>
-                        <p>Explore e experimente a Calculadora Penal hoje mesmo e descubra como podemos facilitar o seu dia a dia no universo do Direito Penal!</p>
+                        <p className="text-base tablet-m:text-sm">Explore e experimente a Calculadora Penal hoje mesmo e descubra como podemos facilitar o seu dia a dia no universo do Direito Penal!</p>
                     </div>
                     <div className="flex gap-5 mobile-g:gap-3">
                         <Link to="/calculator">
@@ -27,7 +27,7 @@ export const Home = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="flex-1 flex flex-col items-center justify-center tablet-m:flex-row tablet-m:flex-none">
+                <div className="flex-1 flex flex-col items-center justify-center tablet-m:flex-row">
                     <Scales className="text-7xl tablet-m:text-5xl mobile-g:text-4xl" weight="thin"/>
                     <h1 className="text-6xl tablet-m:text-4xl mobile-g:text-3xl">Calculadora</h1>
                     <span className="text-6xl tablet-m:text-4xl mobile-g:text-3xl font-extrabold">Penal</span>
